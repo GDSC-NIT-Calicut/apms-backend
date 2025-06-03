d5afa6a42feda4593ed1130a3c6192b022718afc
Nitha Shifana <nithashifana@gmail.com>
2025-06-03 15:51:03 +0530

    backend directory

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Hello from Express + TypeScript + PostgreSQL!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
